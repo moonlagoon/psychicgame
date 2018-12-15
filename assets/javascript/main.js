fd80b008c2a1ca0a4fa277b404ffbe86e804d206
moonlagoon <courtneyroutsong@gmail.com>
2018-12-14 22:57:12 -0500

    cacti boi

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,46 +1,42 @@
-var letters = [
-  'a','b','c','d','e',
-  'f','g','h','i','j',
-  'k','l','m','n','o',
-  'p','q','r','s','t',
-  'u','v','w','x','y','z'];
+var psychicLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 
-  var randomLetter = Math.floor(Math.random() * letters.length;
+var winnerWinnerChickenDinner = 0;
+var booHooBigOleLose = 0;
+
+
+
+var lettersGuessed = []; 
+var remainingAttempts = 8;
+
+document.onkeyup = function(event) {
+  var yourGuess = event.key; 
+  lettersGuessed.push(yourGuess); 
+  var psychicAns = psychicLetters[Math.floor(Math.random() *    psychicLetters.length)];
+
   
-  var guesses = document.getElementById("guesses");
-  var lastResult = document.getElementById("lastResult");
-  var submit = document.getElementById("submit");
-  var guessText = document.getElementById("guessText");
-  var guessCount = 1;
-
-  function checkGuess() {
-    var userGuess = Number(guessText.value);
-    guesses.innerHTML += userGuess + " ";
-
-    if(guessCount == 10) {
-      lastResult.innerHTML = "Lose!";
-      disableForm();
-    } else {
-      
-      if(userGuess == randomLetter) {
-        lastResult.innerHTML = "Win!";
-
-        disableForm();
-      } else {
-        lastResult.innerHTML = "Wrong!";
-        
-      }
-      }
-      guessCount++;
-      guessText.value = "";
-    }
+  if (yourGuess === psychicAns) {
+    winnerWinnerChickenDinner++;
+    remainingAttempts = 8;
+  }
   
-  function disableForm() {
-    var wholeForm = document.querySelector(".form"); 
-    wholeForm.style.opacity = 0.5;
-    guessText.setAttribute("disabled", "disabled");
-    submit.setAttribute("disabled", "disabled");
+  else {
+    remainingAttempts--;
   }
 
-  submit.onclick = checkGuess;
+  
+  if (remainingAttempts === 0){
+    booHooBigOleLose++;
+    remainingAttempts = 8;
+  }
+
+
+
+
+
+  var html = "<h1>Thinking of a letter A-Z:</h1>" + "<h3>Win Count: " +
+   winnerWinnerChickenDinner + "</h3>" + "<h3>Lose Count: "+ booHooBigOleLose +
+    "<h3>Guesses Remaining: " + remainingAttempts + "<h3>Guessed Letters: " +  lettersGuessed.join(', '); 
+  document.querySelector("#psychicgame").innerHTML = html;
+}
